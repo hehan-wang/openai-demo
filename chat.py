00250a5bfd9df35d0acbff7d49114c25340e40b7
wanghehan <454375214@qq.com>
2023-10-26 20:39:44 +0800

    chat

--- chat.py
+++ chat.py
@@ -1,8 +1,8 @@
 import openai
 
 # 设置 OpenAI API 密钥可以给大家免费提供一个调用api中转的key，支持3.5和4.0。额度不多测试是够了。
-# openai.api_key = "sk-NYsoG3VBKDiTuvdtC969F95aFc4f45379aD3854a93602327"
-# openai.api_base="https://key.wenwen-ai.com/v1"
+openai.api_key = "sk-NYsoG3VBKDiTuvdtC969F95aFc4f45379aD3854a93602327"
+openai.api_base="https://key.wenwen-ai.com/v1"
 
 # openai.api_base="https://proxy.hehanwang.com/v1"
 # openai.api_key="sk-xxx"
